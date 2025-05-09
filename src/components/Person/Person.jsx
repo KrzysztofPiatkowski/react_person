682aecc84eabfac39890f50ec505772282086ab0
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-09 06:38:14 +0200

    fix: display age 0 correctly

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,9 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age !== undefined && <p className="Person__age">I am {person.age}</p>}
+    {person.age !== undefined && (
+      <p className="Person__age">I am {person.age}</p>
+    )}
 
     {person.isMarried && person.sex === 'm' && (
       <p className="Person__partner">{person.partnerName} is my wife</p>
